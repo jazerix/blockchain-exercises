75fc5d489f516a1a628bfa8b933cf944bde1abcf
Niels Faurskov <niels@faurskov.dev>
2021-09-16 12:07:18 +0200

    Co-authored-by: Daniel Müller <daniel.m.jorgensen@gmail.com> Hash all the things

---
+++ ProofOfWork/HashCollision.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Proof_of_Work
+{
+    public class HashCollision
+    {
+        public string Target { get; set; } = "000000000";
+
+        public void Collide(string input)
+        {
+            while (!TargetReached(input))
+                input = "0" + input;
+        }
+
+        private bool TargetReached(string input)
+        {
+            var encoded = new List<Byte>(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(input)));
+            var bitString = string.Join("", encoded.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
+            
+            return Target == bitString[..Target.Length];
+        }
+    }
+}
---
+++ ProofOfWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Proof_of_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new List<string>();
+            for (var i = 0; i < 15; i++)
+                strings.Add(RandomString());
+
+            HashCollision h = new HashCollision()
+            {
+                Target = "0000000000000000"
+            };
+            List<TimeSpan> elapsed = new List<TimeSpan>();
+
+            foreach (var s in strings)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                h.Collide(s);
+                sw.Stop();
+                elapsed.Add(sw.Elapsed);
+                Console.WriteLine($"Solution found for: {s}, took {sw.Elapsed.TotalSeconds} seconds");
+            }
+
+            double average = elapsed.Average(x => x.TotalSeconds);
+
+            Console.WriteLine($"Average time: {average} seconds");
+        }
+
+        public static string RandomString()
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 27)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
